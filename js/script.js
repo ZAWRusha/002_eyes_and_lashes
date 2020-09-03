d340703abf9492f09b631540b551b0fd20b0be51
Malkov Dmitry <fly-agaric@bk.ru>
2020-09-04 04:07:11 +0700

    Layout. It remains to bind modal windows to buttons on the pages.

--- js/script.js
+++ js/script.js
@@ -17,11 +17,35 @@ $(document).ready(function() {
 		slidesToShow: 5,
 		slidesToScroll: 1,
 		infinite: true,
-		autoplay: true,
+		autoplay: false,
 		autoplaySpeed: 4000,
 		pauseOnFocus: false,
 		pauseOnHover: true,
-		pauseOnDotsHover: true
+		pauseOnDotsHover: true,
+		responsive: [{
+				breakpoint: 1670,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 1,
+				}
+			},
+			{
+				breakpoint: 1224,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 1
+				}
+			},
+			{
+				breakpoint: 960,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1,
+					centerMode: true,
+					variableWidth: true
+				}
+			}
+		]
 	})
 
 	// $('.slider-nav').slick({
@@ -96,6 +120,46 @@ $(document).ready(function() {
 		$(this).addClass('active');
 	});
 
+	// Сортировка по названию, или цене (Category)
+	$('.sort-name').click(function(e) {
+		e.preventDefault();
+		$(this).parent().parent().find('.sort-cost').removeClass('a-active');
+		$(this).parent().parent().find('.sort-cost').removeClass('sort-active');
+		$(this).addClass('a-active');
+		$(this).addClass('sort-active');
+	});
+	$('.sort-cost').click(function(e) {
+		e.preventDefault();
+		$(this).parent().parent().find('.sort-name').removeClass('a-active');
+		$(this).parent().parent().find('.sort-name').removeClass('sort-active');
+		$(this).addClass('a-active');
+		$(this).addClass('sort-active');
+	});
+
+	// Сортировка по кол-ву страниц (Category)
+	$('.sort-pages').click(function(e) {
+		e.preventDefault();
+		$(this).parent().parent().find('.sort-pages').removeClass('a-active');
+		$(this).addClass('a-active');
+	});
+
+	// Меняет вид карточек (Category)
+	$('.category-sort-type_tile').click(function(e) {
+		e.preventDefault()
+		$(this).parent().find('.category-sort-type_table').removeClass('sort-type_table')
+		$(this).addClass('sort-type_tile')
+		$('.table-item_wrapper').hide()
+		$('.category-item-wrapper').fadeIn()
+	});
+
+	$('.category-sort-type_table').click(function(e) {
+		e.preventDefault()
+		$(this).parent().find('.category-sort-type_tile').removeClass('sort-type_tile')
+		$(this).addClass('sort-type_table')
+		$('.category-item-wrapper').hide()
+		$('.table-item_wrapper').fadeIn()
+	});
+
 
 	$('#photos').slick({
 		slidesToShow: 1,
@@ -135,6 +199,23 @@ $(document).ready(function() {
 	});
 
 	// modal (popup) **************************************
+
+	$('.mob-nav').click(function(e) {
+		e.preventDefault();
+		$('.mobile-block').fadeIn();
+	});
+
+	$('.mob-nav-close').click(function(e) {
+		e.preventDefault();
+		$('.mobile-block').fadeOut();
+	});
+
+	$('li .mob-nav-open').click(function(e) {
+		e.preventDefault();
+		$(this).parent().find('ul').slideToggle();
+		$(this).toggleClass('active');
+	});
+
 
 	//closed all modals
 	$('.modal-bg, .modal-close').click(function(event) {
